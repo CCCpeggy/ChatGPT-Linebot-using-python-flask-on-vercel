13e579b2722cbf118d05fc370665e3ed781f0cab
CCC <32979547+CCCpeggy@users.noreply.github.com>
2025-08-11 18:12:16 +0800

    更新 OpenAI 模型為 gpt-4o

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -9,7 +9,7 @@ client.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default="gpt-4-vision-preview")
+        self.model = os.getenv("OPENAI_MODEL", default="gpt-4o")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default=0.3))
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=2000))
         self.user_portfolio_info = ""
